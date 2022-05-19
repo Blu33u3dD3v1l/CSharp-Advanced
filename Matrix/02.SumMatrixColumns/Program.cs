98e51a7226869b02aa4b34587d434413cb500e8b
Viktor Kachamakov <Kachamakov@abv.bg>
2022-05-19 17:56:54 +0300

    New Homework

---
+++ Matrix/02.SumMatrixColumns/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SumMatrixColumns
+{
+    class Program
+    {
+        static void Main()
+        {
+            var dimention = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            var matrix = new int[dimention[0], dimention[1]];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = numbers[col];
+
+                }
+            }
+
+
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                var a = 0;
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    a += matrix[row, col];
+                }
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
+
