8fb9ef988f4a51f2b279051030262b291f0305b3
Viktor Kachamakov <Kachamakov@abv.bg>
2022-05-21 00:31:30 +0300

    Solution Added!

---
+++ Matrix/03.Primary Diagonal/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            var num = int.Parse(Console.ReadLine());
+            var matrix = new int[num, num];
+
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var line = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = line[col];
+
+
+                }
+            }
+            var a = 0;
+
+            for (int i = 0; i < num; i++)
+            {
+                a += matrix[i, 0 + i];
+            }
+            Console.WriteLine(a);
+        }
+
+    }
+}
