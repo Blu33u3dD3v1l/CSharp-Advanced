c8c94b5e5f73cbd8962f1503d347a86c15915f65
Viktor Kachamakov <Kachamakov@abv.bg>
2022-05-22 21:45:52 +0300

    Homework

---
+++ Queues and Stacks/01.Fast Food/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FstFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
