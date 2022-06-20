0519e90c08a13941ac9c74b53b7b7d8c56c030a9
Viktor Kachamakov <Kachamakov@abv.bg>
2022-06-20 19:08:40 +0300

    Adding solution

---
+++ Queues and Stacks/12. Scheduling/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Sheduling
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var line = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            var line1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var numToKill = int.Parse(Console.ReadLine()); 
+
+            var task = new Stack<int>(line);
+            var thread = new Queue<int>(line1);
+
+            var some = 0;
+
+            while (task.Any() && thread.Any())
+            {
+                var taskFirst = task.Peek();
+                var threadFirst = thread.Peek();
+                if (taskFirst == numToKill)
+                {
+                    some = threadFirst;
+                    break;
+                }
+                if (threadFirst >= taskFirst)
+                {
+                    thread.Dequeue();
+                    task.Pop();
+                }
+                if(threadFirst < taskFirst)
+                {
+                    thread.Dequeue();
+                } 
+
+            }
+            Console.WriteLine($"Thread with value {some} killed task {numToKill}");
+            Console.WriteLine(String.Join(" ",thread));
+            
+
+        }
+    }
+}
