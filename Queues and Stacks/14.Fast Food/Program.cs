78c6cb899ed91fc58a4f4f65597b5b07484a7b18
Viktor Kachamakov <Kachamakov@abv.bg>
2022-06-23 16:19:55 +0300

    Solution

---
+++ Queues and Stacks/14.Fast Food/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace FastFood
+{
+    public class Program
+    {
+        static void Main()
+        {
+
+            var num = int.Parse(Console.ReadLine());
+            var line = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var orders = new Queue<int>(line);
+            var a = orders.Max();
+
+            while (true)
+            {
+                if(orders.Count > 0)
+                {
+                    var order = orders.Peek();
+                    if (num >= order)
+                    {
+                        num -= order;
+                        orders.Dequeue();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }            
+                else
+                {
+                    break;
+                }
+            }
+            if(orders.Count == 0)
+            {
+                Console.WriteLine(a);
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine(a);
+                Console.WriteLine($"Orders left: {string.Join(" ",orders)}");
+            }
+
+        }
+     
+    }
+}
