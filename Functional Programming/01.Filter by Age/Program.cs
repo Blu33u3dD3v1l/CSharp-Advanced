8e58f1e0af6f6f4ab9dfaf778ea571761ca4a078
Viktor Kachamakov <Kachamakov@abv.bg>
2022-06-24 12:00:44 +0300

    Added solution!

---
+++ Functional Programming/01.Filter by Age/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SortingByAge
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            var num = int.Parse(Console.ReadLine());
+            var dict = new Dictionary<string, int>();
+            for (int i = 0; i < num; i++)
+            {
+                var line = Console.ReadLine();
+                var tokens = line.Split(", ");
+                var name = tokens[0];
+                var agee = int.Parse(tokens[1]);
+                if (!dict.ContainsKey(name))
+                {
+                    dict.Add(name, agee);
+                }
+
+            }
+            var command = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+            var format = Console.ReadLine();
+
+            if (command == "older")
+            {
+                if (format == "name age")
+                {
+                    foreach (var item in dict.Where(x => x.Value >= age))
+                    {
+                        Console.WriteLine($"{item.Key} - {item.Value}");
+                    }
+                }
+                else if (format == "name")
+                {
+                    foreach (var item in dict.Where(x => x.Value >= age))
+                    {
+                        Console.WriteLine($"{item.Key}");
+                    }
+                }
+                else
+                {
+                    foreach (var item in dict.Where(x => x.Value >= age))
+                    {
+                        Console.WriteLine($"{item.Value}");
+                    }
+                }
+            }
+            if (command == "younger")
+            {
+                if (format == "name age")
+                {
+                    foreach (var item in dict.Where(x => x.Value < age))
+                    {
+                        Console.WriteLine($"{item.Key} - {item.Value}");
+                    }
+                }
+                else if (format == "name")
+                {
+                    foreach (var item in dict.Where(x => x.Value < age))
+                    {
+                        Console.WriteLine($"{item.Key}");
+                    }
+                }
+                else
+                {
+                    foreach (var item in dict.Where(x => x.Value < age))
+                    {
+                        Console.WriteLine($"{item.Value}");
+                    }
+                }
+            }
+
+        }
+    }
+}
