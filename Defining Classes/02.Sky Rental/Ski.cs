3984c65b8412ad4e2b3d47b2caa51fa29b8793e9
Viktor Kachamakov <Kachamakov@abv.bg>
2022-06-29 20:26:40 +0300

    My Solution for old Exam!

---
+++ Defining Classes/02.Sky Rental/Ski.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkiRental
+{
+    public class Ski
+    {
+        public string manufacturer;
+        public string model;
+        public int year;
+
+        public Ski(string manufacturer, string model, int year)
+        {
+            Manufacturer = manufacturer;
+            Model = model;
+            Year = year;
+
+        }
+
+        public string Manufacturer
+        {
+            get { return manufacturer; }
+            set { manufacturer = value; }
+        }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+
+        }
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+        public override string ToString()
+        {
+
+
+            var some = $"{manufacturer} - {model} - {year}";
+            return some;
+        }
+    }
+}
+
+
---
+++ Defining Classes/02.Sky Rental/SkiRental.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SkiRental
+{
+    public class SkiRental
+    {
+        List<Ski> data = new List<Ski>();
+
+        public string name;
+        public int capacity;
+
+
+        public SkiRental(string name, int capacity)
+        {
+
+            Name = name;
+            Capacity = capacity;
+
+        }
+
+        public int Count
+        {
+            get { return data.Count; }
+
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Capacity
+        {
+
+            get { return capacity; }
+            set { capacity = value; }
+        }
+        public void Add(Ski Ski)
+        {
+            if (data.Count < capacity)
+            {
+                data.Add(Ski);
+            } 
+       }    
+        public bool Remove(string manufacturer, string model)
+        {
+            if(data.Count > 0)
+            {
+                var some = data.FirstOrDefault(x => x.Manufacturer == manufacturer && x.Model == model);
+                if (some != null)
+                {
+                    data.Remove(some);
+                    return true;
+                }
+               
+            }
+            return false;
+          
+         
+        }
+        public Ski GetNewestSki()
+        {
+            if(data.Count > 0)
+            {
+                var some = data.Max(x => x.year);
+                var thisThing = data.First(x => x.year == some);
+                return thisThing;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public Ski GetSki(string manufacturer, string model)
+        {
+            if(data.Count > 0)
+            {
+                var thisThing = data.FirstOrDefault(x => x.Manufacturer == manufacturer && x.Model == model);
+                if(thisThing != null)
+                {
+                    return thisThing;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            return null;
+        }
+        public string GetStatistics()
+        {
+
+            var sb = new StringBuilder();
+            sb.AppendLine($"The skis stored in {Name}:");
+            foreach(var item in data)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            return sb.ToString();
+           
+        }
+
+    }
+}
